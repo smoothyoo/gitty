a878e912170a37e87a42143648c2d8b7fc5451ef
qudvkfzoqtyd <77221619+qudvkfzoqtyd@users.noreply.github.com>
2026-02-11 17:23:32 +0900

    Revert "feat: 관리자 페이지 추가"
    This reverts commit 199d5d288ee486d796ffa21493a1b74a1b03a6f7.

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import SignupPage from './pages/SignupPage'
 import LoginPage from './pages/LoginPage'
 import HomePage from './pages/HomePage'
 import ProfilePage from './pages/ProfilePage'
-import AdminPage from './pages/AdminPage'
 
 function App() {
   return (
@@ -17,7 +16,6 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/home" element={<HomePage />} />
           <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/admin" element={<AdminPage />} />
         </Routes>
       </AuthProvider>
     </BrowserRouter>
